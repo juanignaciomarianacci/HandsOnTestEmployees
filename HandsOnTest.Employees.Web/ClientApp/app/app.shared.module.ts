e3e737a30915629599640b7c8780fca6b431825d
Juan Marianacci <Juan.Marianacci@Coalfire.com>
2021-02-06 12:40:08 -0300

    Cleaning up of unused classes and project

--- HandsOnTest.Employees.Web/ClientApp/app/app.shared.module.ts
+++ HandsOnTest.Employees.Web/ClientApp/app/app.shared.module.ts
@@ -7,16 +7,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { EmployeeDataComponent } from './components/employee/employee.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent,
         EmployeeDataComponent
     ],
@@ -27,8 +23,6 @@ import { EmployeeDataComponent } from './components/employee/employee.component'
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: 'employee', component: EmployeeDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
